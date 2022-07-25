342b4d5338d04ceeaa25515071eed5ceca39cb58
Daniel <43097521+Hpro1@users.noreply.github.com>
2022-07-25 13:36:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/softwaredevelopmentskills/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.softwaredevelopmentskills;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroup);
+
+        Button addButton = (Button) findViewById(R.id.addButton);
+        Button mainButton = (Button) findViewById(R.id.homeButton);
+        addButton.setOnClickListener(this::onClick);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+
+    public void checkButton(View v) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+
+        Toast.makeText(this, "Selected operator: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void onClick(View v) {
+        EditText firstNumberEditText = findViewById(R.id.firstNumberEditText);
+        EditText secondNumberEditText = (EditText) findViewById(R.id.secondNumberEditText);
+        TextView resultTextView;
+        resultTextView = findViewById(R.id.resultTextView);
+
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+
+        if (radioButton.getText().equals("Add")) {
+
+            float num1 = Float.parseFloat(firstNumberEditText.getText().toString());
+            float num2 = Float.parseFloat(secondNumberEditText.getText().toString());
+            float result = num1 + num2;
+
+            resultTextView.setText(String.format(Locale.ROOT, "%.2f", result) + "");
+        } else if (radioButton.getText().equals("Subtract")) {
+
+            float num1 = Float.parseFloat(firstNumberEditText.getText().toString());
+            float num2 = Float.parseFloat(secondNumberEditText.getText().toString());
+            float result = num1 - num2;
+
+            resultTextView.setText(String.format(Locale.ROOT, "%.2f", result) + "");
+        } else if (radioButton.getText().equals("Multiply")) {
+
+            float num1 = Float.parseFloat(firstNumberEditText.getText().toString());
+            float num2 = Float.parseFloat(secondNumberEditText.getText().toString());
+            float result = num1 * num2;
+
+            resultTextView.setText(String.format(Locale.ROOT, "%.2f", result) + "");
+        } else {
+
+            float num1 = Float.parseFloat(firstNumberEditText.getText().toString());
+            float num2 = Float.parseFloat(secondNumberEditText.getText().toString());
+            float result = num1 / num2;
+
+            resultTextView.setText(String.format(Locale.ROOT, "%.2f", result) + "");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/softwaredevelopmentskills/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.softwaredevelopmentskills;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button calcActivityButton = (Button) findViewById(R.id.buttonCalculator);
+        calcActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+}
